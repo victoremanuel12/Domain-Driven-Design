fa0dc925e17029448f737f9e7011ef6e1bfcc1ce
victoremanuel12 <93943734+victoremanuel12@users.noreply.github.com>
2025-10-18 01:04:52 -0300

    feat? explica;'ao do domain event

--- src/Wpm.SharedKerbel/DomainEvent/DomainEventDispatcher.cs
+++ src/Wpm.SharedKerbel/DomainEvent/DomainEventDispatcher.cs
@@ -4,13 +4,16 @@ namespace Wpm.SharedKerbel.DomainEvent
 {
     public class DomainEventDispatcher<T> where T : IDomainEvent
     {
+        //lista de ações (Actions) que querem ser notificadas quando um evento específico acontecer.
         private List<Action<T>> Actions { get; } = new();
         public void Subscribe(Action<T> action)
         {
-            if (Actions.Exists(a => a.Method == action.Method)) 
+            //Verifica se a ação já está registrada, evita duplicar a mesma assinatura várias vezes.
+            if (Actions.Exists(a => a.Method == action.Method))
                 return;
-            Actions.Add(action);    
+            Actions.Add(action);
         }
+        //Quando Publish() é chamado, ele executa todas as ações registradas.
         public void Publish(T domainEvent)
         {
             foreach (var action in Actions)
